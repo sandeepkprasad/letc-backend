80b0559fa1c3edc7d38875006e81c8ff16c73129
Sandeep Kumar <sandeepkprasad@outlook.in>
2023-09-08 23:17:43 +0530

    final

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = () => {
   let URI =
-    "mongodb+srv://sandeepkprasad:success2022@mycluster.s12lxcc.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb+srv://rcsmongodatabase:rcsmongodbdatabase@rcscluster.jqzczzp.mongodb.net/?retryWrites=true&w=majority";
 
   mongoose.connect(URI);
 };
